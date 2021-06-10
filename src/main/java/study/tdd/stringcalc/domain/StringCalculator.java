d2a2f436fd7139653eeb35800c3e0ea2577bb3dc
pjhsk113 <pjhsk113@naver.com>
2021-06-10 21:47:59 +0900

    [refactor] 계산 로직 private 메서드로 변경

--- src/main/java/study/tdd/stringcalc/domain/StringCalculator.java
+++ src/main/java/study/tdd/stringcalc/domain/StringCalculator.java
@@ -1,16 +1,20 @@
 package study.tdd.stringcalc.domain;
 
-import org.springframework.util.StringUtils;
 import study.tdd.stringcalc.exception.ExceptionMessage;
 import study.tdd.stringcalc.util.StringUtil;
 
 public class StringCalculator {
 
+    public static int calculateResult(String input) {
+        isBlank(input);
+        return calculate(StringUtil.split(input));
+    }
+
     /**
      * Operator.result(operator, firstValue, secondValue))
      * 연산은 Operator로 위임한다.
      */
-    public static int calculate(String[] splitInput) {
+    private static int calculate(String[] splitInput) {
         int result = StringUtil.toInt(splitInput[0]);
         for (int i = 2; i < splitInput.length; i += 2) {
             result = Operator.result(splitInput[i-1], result, StringUtil.toInt(splitInput[i]));
@@ -18,14 +22,9 @@ public class StringCalculator {
         return result;
     }
 
-    public static int calculateResult(String input) {
-        isBlank(input);
-        return calculate(StringUtil.split(input));
-    }
-
     /* validation check*/
-    public static void isBlank(String input) {
-        if (StringUtils.isEmpty(input) || input.equals(" ")) {
+    private static void isBlank(String input) {
+        if (input.isEmpty() || input.equals(" ")) {
             throw new IllegalArgumentException(ExceptionMessage.INPUT_VALUE_EMPTY);
         }
     }
